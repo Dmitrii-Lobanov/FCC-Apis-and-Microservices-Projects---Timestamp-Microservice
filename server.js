5bbdac125fd9b9479e0ed269af46278a571e64dd
Dmitrii Lobanov <41771694+Dmitrii-Lobanov@users.noreply.github.com>
2019-07-24 15:09:30 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+// Get dependencies
+const express = require('express');
+const http = require("http");
+const app = express();
+
+// Get API routes 
+let api = require('./api.js');
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
